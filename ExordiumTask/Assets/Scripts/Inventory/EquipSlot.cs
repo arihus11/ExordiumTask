269b4f25ac98b00d30d2362f5c41784afa35d25d
d_arih <ja.jakovac@gmail.com>
2022-03-16 00:48:38 +0100

    Add scripts for equip ui and slots skelet
    Add basic functions into equip slots scripts and functions for turning on and off equip slots.

--- ExordiumTask/Assets/Scripts/Inventory/EquipSlot.cs
+++ ExordiumTask/Assets/Scripts/Inventory/EquipSlot.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EquipSlot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public GameObject icon;
+    Item item;
+
+    public void EquipItem(Item newItem)
     {
-        
+        item = newItem;
+
+        icon.gameObject.GetComponent<SpriteRenderer>().sprite = item.icon;
+        icon.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UnequipItem()
     {
-        
+        item = null;
+
+        icon.gameObject.GetComponent<SpriteRenderer>().sprite = null;
+        icon.gameObject.GetComponent<SpriteRenderer>().enabled = false;
     }
 }
---
+++ ExordiumTask/Assets/Scripts/Inventory/EquipUi.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipUi : MonoBehaviour
+{
+    //  Inventory inventory;
+    //   public Transform itemsParent;
+
+    public GameObject equipUI;
+    // EquipSlot[] slots;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("EquipPanel"))
+        {
+            equipUI.SetActive(!equipUI.activeSelf);
+        }
+    }
+
+    void UpdateUI()
+    {
+
+    }
+}
